4e958d8d73c50907d649120eb74ed256394b0619
Félix Arnoult <fel.arnoult@gmail.com>
2020-02-11 12:36:45 +0100

    Correction import

--- image-getter/fetchImageFromQwant.py
+++ image-getter/fetchImageFromQwant.py
@@ -7,6 +7,8 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import urllib
+import urllib.request
+>>>>>>> 468f32e... Correction import
 import random
 
 defaultNumberOfImage = 5
